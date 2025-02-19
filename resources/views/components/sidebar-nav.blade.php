83325f770c574885367fd78e72b8219e2dc2a9e3
Uliana Bulygina <ulanabulygina5@gmail.com>
2025-02-19 14:17:51 +0500

    по маршруту /theory отображается страница с теорией и слайдбаром

---
+++ resources/views/components/sidebar-nav.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <h2 class="font-bold text-lg mb-2">Навигация</h2>
+    <ul class="list-disc pl-5">
+        <li><a href="#section1" class="text-blue-500 hover:underline">Раздел 1</a></li>
+        <li><a href="#section2" class="text-blue-500 hover:underline">Раздел 2</a></li>
+        <li><a href="#section3" class="text-blue-500 hover:underline">Раздел 3</a></li>
+        <li><a href="#section4" class="text-blue-500 hover:underline">Раздел 4</a></li>
+    </ul>
+</div>
